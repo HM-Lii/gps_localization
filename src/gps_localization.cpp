06a39793804027f2167d8334e3839a76f05e2eb3
Li Haomin <1207638940@qq.com>
2023-12-18 05:19:57 -0800

    优化代码，并增加功能：在使用fix来reset墨卡托投影前等待十次，避免Gps双天线刚启动时的大幅度漂移

--- src/gps_localization.cpp
+++ src/gps_localization.cpp
@@ -1,29 +1,22 @@
 #include <ros/ros.h>
-#include <ros/spinner.h>
-#include <sensor_msgs/NavSatFix.h>
-#include <geometry_msgs/Pose2D.h>
-#include <geometry_msgs/PoseStamped.h>
-#include <nav_msgs/Path.h>
-#include <nav_msgs/Odometry.h>
 #include <geometry_msgs/TwistStamped.h>
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2/LinearMath/Transform.h>
+#include <sensor_msgs/NavSatFix.h>
+#include <geometry_msgs/Pose2D.h>
+#include <nav_msgs/Odometry.h>
+#include <nav_msgs/Path.h>
 #include <GeographicLib/Geocentric.hpp>
 #include <GeographicLib/LocalCartesian.hpp>
-#include <GeographicLib/Constants.hpp>
-
-#include<mqtt_com/mqtt_com.h>
+#include <mqtt_com/mqtt_com.h>
 
 using namespace GeographicLib;
 
-// 定义GPS定位类
 class GpsLocalization {
 public:
-    // 构造函数
     GpsLocalization() : is_first_fix(true), gps_heading(0), enu_heading(0), body_dx(0), body_dy(0),
                         earth_model(Constants::WGS84_a(), Constants::WGS84_f()) {
         ros::NodeHandle nh;
-        // 获取参数
         nh.param("gps_localization/body_dx", body_dx, 0.0);
         nh.param("/gps_localization/body_dy", body_dy, 0.0);
 
@@ -37,11 +30,9 @@ public:
         path.header.frame_id = "camera_init";
     }
 
-    // 主循环
     void spin() {
-        ros::AsyncSpinner spinner(4);  // 创建一个AsyncSpinner对象，指定使用4个线程
-        spinner.start();  // 开始多线程处理
-
+        ros::AsyncSpinner spinner(4);  
+        spinner.start(); 
         ros::Rate rate(1);
         while(ros::ok())
         {
@@ -49,20 +40,20 @@ public:
             MqttCom_.publish("gecao",latitude,longitude,vel,100);
             rate.sleep();
         }
-
         spinner.stop();
     }
+
 private:
-    bool is_first_fix;  // 是否为第一次定位
-    double gps_heading, enu_heading ,latitude,longitude,vel,body_dx, body_dy;  // GPS航向，ENU航向，体坐标系下的dx和dy
-    Geocentric earth_model;  // 地球模型
-    LocalCartesian local_projector;  // 本地投影器
-    nav_msgs::Path path;  // 路径
+    bool is_first_fix;
+    int wait_count=0;
+    double gps_heading, enu_heading ,latitude,longitude,vel,body_dx, body_dy;
+    Geocentric earth_model;  
+    LocalCartesian local_projector; 
+    nav_msgs::Path path; 
     ros::Publisher odometry_publisher, path_publisher;  
     ros::Subscriber fix_subscriber, heading_subscriber,vel_subscriber;  
     MqttCom MqttCom_;
 
-    // 更新航向
     void headingCallback(const geometry_msgs::Pose2DPtr &msg) {
         gps_heading = msg->theta;
         enu_heading = 90 - gps_heading;
@@ -71,12 +62,30 @@ private:
         }
     }
 
-    // 处理定位信息
     void fixCallback(const sensor_msgs::NavSatFix::ConstPtr& msg) {
-        if (is_first_fix) {
-            local_projector.Reset(msg->latitude, msg->longitude, msg->altitude);
-            is_first_fix = false;
+      if (is_first_fix) {
+        if (wait_count != 10) {
+          wait_count++;
+          return;
         }
+        local_projector.Reset(msg->latitude, msg->longitude, msg->altitude);
+        is_first_fix = false;
+      }
+        updateMqttMsg(msg);
+        double x, y, z;
+        local_projector.Forward(msg->latitude, msg->longitude, msg->altitude, x, y, z);
+        tf2::Transform transform = getTransform();
+        tf2::Vector3 enu_offset = getEnuOffset(transform);
+        x += enu_offset.getX();
+        y += enu_offset.getY();
+        publishOdometry(x, y, z);
+    }
+
+    void velCallback(const geometry_msgs::TwistStamped::ConstPtr& msg) {
+        updateVelocity(msg);
+    }
+
+    void updateMqttMsg(const sensor_msgs::NavSatFix::ConstPtr& msg) {
         static int j=10;
         if(j==10){
             j=0;
@@ -84,46 +93,50 @@ private:
             longitude=msg->longitude;  
         }
         j++;
-        double x, y, z;
-        local_projector.Forward(msg->latitude, msg->longitude, msg->altitude, x, y, z);
+    }
 
+    tf2::Transform getTransform() {
         tf2::Transform transform;
         transform.setOrigin(tf2::Vector3(0, 0, 0));
         tf2::Quaternion quaternion_;
         quaternion_.setRPY(0, 0, enu_heading * M_PI / 180.0);
         transform.setRotation(quaternion_);
+        return transform;
+    }
 
+    tf2::Vector3 getEnuOffset(tf2::Transform &transform) {
         tf2::Vector3 body_offset(body_dx, body_dy, 0.0);
-        tf2::Vector3 enu_offset = transform * body_offset;
+        return transform * body_offset;
+    }
 
-        x += enu_offset.getX();
-        y += enu_offset.getY();
-
+    void publishOdometry(double x, double y, double z) {
         geometry_msgs::Pose pose;
         pose.position.x = x;
         pose.position.y = y;
         pose.position.z = z;
-
         tf2::Quaternion quaternion;
         quaternion.setRPY(0, 0, enu_heading * M_PI / 180.0);
         pose.orientation.x = quaternion.x();
         pose.orientation.y = quaternion.y();
         pose.orientation.z = quaternion.z();
         pose.orientation.w = quaternion.w();
-
         nav_msgs::Odometry odometry;    
         odometry.pose.pose = pose;
         odometry.header.stamp = ros::Time::now();
         odometry.header.frame_id = "camera_init";
         odometry_publisher.publish(odometry);
+        publishPath(pose);
+    }
 
+    void publishPath(geometry_msgs::Pose &pose) {
         geometry_msgs::PoseStamped pose_stamped;
         pose_stamped.pose = pose;
         pose_stamped.header.stamp = ros::Time::now();
         pose_stamped.header.frame_id = "camera_init";
         path.poses.push_back(pose_stamped);
     }
-    void velCallback(const geometry_msgs::TwistStamped::ConstPtr& msg) {
+
+    void updateVelocity(const geometry_msgs::TwistStamped::ConstPtr& msg) {
         static int i=10;
         if(i==10){
             i=0;
@@ -133,7 +146,6 @@ private:
     }
 };
 
-// 主函数
 int main(int argc, char** argv) {
     ros::init(argc, argv, "gps_localization");
     GpsLocalization gpsLocalization;
